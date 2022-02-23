cc5ac3f50337ae435099719197562574f7f43933
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:17:29 -0800

    init nav logo

---
+++ shared/components/navbar/components/NavbarLogo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+export const NavbarLogo = () => {
+  return (
+    <Heading size="md">
+      <Flex alignItems={"baseline"}>
+        <Text color="telegram.600" fontSize={"4xl"}>
+          Medi
+        </Text>
+        <Text fontSize={"larger"} color="whiteAlpha.900">
+          Care
+        </Text>
+      </Flex>
+    </Heading>
+  );
+};
