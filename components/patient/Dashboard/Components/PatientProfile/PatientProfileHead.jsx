de44b8e7a07f0b72f49971db30f50aa92fa2b126
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:09:30 -0800

    init patient profile head

---
+++ components/patient/Dashboard/Components/PatientProfile/PatientProfileHead.jsx
@@ -0,0 +1,48 @@
+import {
+  Avatar,
+  Flex,
+  Heading,
+  Text,
+  Box,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
+import React from "react";
+
+export const PatientProfileHead = ({ patient, appointmentStats }) => {
+  const { firstName, lastName, email } = patient;
+  return (
+    <Flex flexDirection={"column"}>
+      <Flex flexDirection={"column"} alignItems="center">
+        <Avatar size="xl" name="Ryan Florence" />
+        <Heading size={"md"} mt="2" color="whiteAlpha.900">
+          {`${firstName} ${lastName}`}
+        </Heading>
+        <Text fontSize={"sm"} color="whiteAlpha.900">
+          {email}
+        </Text>
+      </Flex>
+      <Flex justifyContent={"space-between"} mt="6" gap={"4"}>
+        <Flex flexDirection={"column"} alignItems="center">
+          <Box fontWeight={"bold"} color="whiteAlpha.900">
+            {appointmentStats.numberOfAppointments}
+          </Box>
+          <Text fontSize={"xs"} color="whiteAlpha.900">
+            appointments
+          </Text>
+        </Flex>
+        <Center height="50px">
+          <Divider orientation="vertical" colorScheme={"red"} />
+        </Center>
+        <Flex flexDirection={"column"} alignItems="center">
+          <Box fontWeight={"bold"} color="whiteAlpha.900">
+            {appointmentStats.upComming}
+          </Box>
+          <Box fontSize={"xs"} color="whiteAlpha.900">
+            Upcoming
+          </Box>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
