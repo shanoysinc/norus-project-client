d4a0f9b3f2ebc79f053eb261d8a7c30ed743ac47
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:17:16 -0800

    init global navbar

---
+++ shared/components/navbar/UserNavBar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  Flex,
+  Box,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider,
+  Avatar,
+} from "@chakra-ui/react";
+import { NavbarLogo } from "./components";
+import { useMutation } from "react-query";
+import { useRouter } from "next/router";
+import { baseApiClient } from "../../../lib/axios/baseApiClient";
+import { useAuth } from "../../../hooks";
+import { getUserToken } from "../../../lib/getUserToken";
+
+export const UserNavBar = ({ name }) => {
+  const { auth } = useAuth();
+  const router = useRouter();
+
+  const mutation = useMutation(() => baseApiClient.post("/user/logout"), {
+    headers: {
+      authorization: `Bearer ${getUserToken(auth)}`,
+    },
+    onSuccess: () => {
+      localStorage.removeItem("token");
+      router.push("/");
+    },
+    onError: () => {
+      router.push("/");
+    },
+  });
+
+  const logOutHandler = () => {
+    localStorage.removeItem("token");
+
+    mutation.mutate();
+  };
+  return (
+    <Flex
+      bg="gray.800"
+      paddingX={"6"}
+      paddingY={"3"}
+      justifyContent={"space-between"}
+      alignItems="center"
+    >
+      <Flex alignItems="baseline">
+        <Box pr={"6"}>
+          <NavbarLogo />
+        </Box>
+      </Flex>
+      <Menu>
+        <MenuButton>
+          <Avatar size="md" name={name} />
+        </MenuButton>
+        <MenuList>
+          <MenuGroup title="Profile">
+            <MenuItem>Edit Profile</MenuItem>
+            <MenuItem onClick={logOutHandler}>Log out</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="Help">
+            <MenuItem>Docs</MenuItem>
+            <MenuItem>FAQ</MenuItem>
+          </MenuGroup>
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
