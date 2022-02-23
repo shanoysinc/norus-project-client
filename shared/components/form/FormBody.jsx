0dc0687aa5d7d052506c576ca1a074cfd2b3f6cf
shanoysinc <shanoy96@gmail.com>
2022-02-23 09:16:19 -0800

    add isRequired boolean

--- shared/components/form/FormBody.jsx
+++ shared/components/form/FormBody.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { FormControl } from "@chakra-ui/react";
 
-export const FormBody = ({ children, isInvalid }) => {
+export const FormBody = ({ children, isInvalid, isRequired }) => {
   return (
     <FormControl
       marginTop={"6"}
       padding={"8"}
       borderRadius={"6"}
-      isRequired
+      isRequired={isRequired}
       bg="gray.50"
-      width={"xs"}
+      width={"md"}
       boxShadow="md"
       isInvalid={isInvalid}
     >
