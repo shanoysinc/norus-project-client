0a5b698f031148beb56323803a417d5bc82e26b2
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:10:24 -0800

    create auth hook

---
+++ hooks/auth/useAuth.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../Providers";
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error("useAuth must be nested inside of a AuthProvider");
+  }
+  return context;
+};
+
+export default useAuth;
