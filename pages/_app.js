6321cba1a2463e615e95df87f5148ad8c5fa2d04
shanoysinc <shanoy96@gmail.com>
2022-02-22 23:11:11 -0800

    add auth provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "../Providers";
+import { RootApp } from "../components/App/RootApp";
 
 const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <AuthProvider>
+          <RootApp pageProps={pageProps} Component={Component} />
+        </AuthProvider>
       </QueryClientProvider>
     </ChakraProvider>
   );
