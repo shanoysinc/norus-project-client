49a6c6b551ef98954b9a48872146d26ce542160b
shanoysinc <shanoy96@gmail.com>
2022-02-27 00:29:24 -0800

    make header a link

--- shared/components/form/formHeader.jsx
+++ shared/components/form/formHeader.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import { Heading, Text, Flex } from "@chakra-ui/react";
+import { Heading, Text, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+
 export const FormHeader = ({ subTitle }) => {
   return (
-    <Flex flexDirection={"column"}>
-      <Heading size="2xl">
-        <Flex>
-          <Text color="telegram.600" fontSize={"larger"}>
-            Medi
-          </Text>
-          <Text fontSize={"larger"} color="gray.700">
-            Care
-          </Text>
+    <NextLink href={"/"} passHref>
+      <Link color="gray.600">
+        <Flex flexDirection={"column"}>
+          <Heading size="2xl">
+            <Flex>
+              <Text color="telegram.600" fontSize={"larger"}>
+                Medi
+              </Text>
+              <Text fontSize={"larger"} color="gray.700">
+                Care
+              </Text>
+            </Flex>
+          </Heading>
+          <Heading size="sm" color="gray.700">
+            {subTitle}
+          </Heading>
         </Flex>
-      </Heading>
-      <Heading size="sm" color="gray.700">
-        {subTitle}
-      </Heading>
-    </Flex>
+      </Link>
+    </NextLink>
   );
 };
