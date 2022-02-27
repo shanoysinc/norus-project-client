d6b489cd8cbbafe398a78d3e16d7ef977b396443
shanoysinc <shanoy96@gmail.com>
2022-02-26 23:51:26 -0800

    add timeline date and time

--- components/patient/Dashboard/Components/timeline/TimlineEvent.jsx
+++ components/patient/Dashboard/Components/timeline/TimlineEvent.jsx
@@ -1,19 +1,23 @@
 import { Badge, Text } from "@chakra-ui/react";
+import { format, parseISO } from "date-fns";
 import React from "react";
 import { Event } from "react-trivial-timeline";
 
-export const TimlineEvent = ({ approve, symptom }) => {
+export const TimlineEvent = ({ approve, symptom, createdAt }) => {
   return (
     <>
       <Event
-        interval={{ start: 2010 }}
-        lineColor="#007AB8;"
+        interval={{
+          start: format(new Date(createdAt), "MMM dd, yyyy."),
+          end: format(parseISO(createdAt), "h:mm a"),
+        }}
+        lineColor="#0BC5EA"
         intervalBackground={"black"}
         intervalColor="white"
         title={""}
       >
         <Text color="white">
-          {symptom}
+          Appointment for {symptom} -
           <Badge ml="4" colorScheme={approve ? "green" : "red"}>
             {approve ? "Approve" : "Decline"}
           </Badge>
