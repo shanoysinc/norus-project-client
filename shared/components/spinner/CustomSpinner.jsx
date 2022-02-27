613894e3b3801628236d81ac6d90b5762d21f136
shanoysinc <shanoy96@gmail.com>
2022-02-27 01:00:53 -0800

    custom spinner

--- shared/components/spinner/CustomSpinner.jsx
+++ shared/components/spinner/CustomSpinner.jsx
@@ -4,9 +4,11 @@ import { Box, Center, Flex, Spinner, Text } from "@chakra-ui/react";
 export const CustomSpinner = ({
   message = "Loading....",
   color = "gray.800",
+  left = "50%",
+  top = "15em",
 }) => {
   return (
-    <Box position={"absolute"} top="15em" left={"50%"}>
+    <Box position={"absolute"} top={top} left={left}>
       <Flex flexDirection={"column"}>
         <Spinner
           thickness="4px"
@@ -14,6 +16,7 @@ export const CustomSpinner = ({
           emptyColor="gray.200"
           color="blue.500"
           size="xl"
+          ml="10"
         />
         <Center>
           <Text color={color}>{message}</Text>
