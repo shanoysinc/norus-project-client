a137cd1e05865b8f4184a6a26ecf50a38bb072c0
shanoysinc <shanoy96@gmail.com>
2022-03-08 21:51:19 -0800

    add appointment modal

--- components/doctor/dashboard/components/DoctorsPatient.jsx
+++ components/doctor/dashboard/components/DoctorsPatient.jsx
@@ -1,9 +1,10 @@
-import { Avatar, Box, Flex, Icon, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Icon, Text, useDisclosure } from "@chakra-ui/react";
 import { format, isAfter, parseISO, subDays } from "date-fns";
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import { useMutation, useQueryClient } from "react-query";
 import { baseApiClient } from "../../../../lib/axios/baseApiClient";
+import { DoctorPatientModal } from "./DoctorPatientModal";
 import { DoctorsPatientBadge } from "./DoctorsPatientBadge";
 
 export const DoctorsPatient = ({
@@ -14,7 +15,11 @@ export const DoctorsPatient = ({
   token,
   approve,
   date,
+  details,
 }) => {
+  // console.log("details", details);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const yeserdayDate = subDays(new Date(), 1);
   const isPastAppointment = isAfter(new Date(date), yeserdayDate);
 
@@ -37,47 +42,59 @@ export const DoctorsPatient = ({
     mutation.mutate({ currentAppointmentId: id, approve });
   };
   return (
-    <Box mt="6" bg="gray.700" p="4" borderRadius={"4"}>
-      <Flex justifyContent="space-between">
-        <Box>
-          <Flex>
-            <Avatar name={name} />
-            <Box pl="2" pt="1">
-              <Text fontSize={"sm"}>{name}</Text>
-              <Text fontSize={"xs"} color="skyblue">
-                {symptom}
-              </Text>
-            </Box>
-          </Flex>
-        </Box>
-        <Box pl="5">
-          <Flex alignItems={"center"}>
-            {isPastAppointment ? (
-              <Icon
-                cursor={"pointer"}
-                onClick={updateAppointment}
-                color={approve ? "green.300" : "red.300"}
-                as={FaCheckCircle}
-                w={8}
-                h={8}
-                mt="1.5"
-              />
-            ) : (
-              <DoctorsPatientBadge approve={approve} />
-            )}
-            <Box ml="6">
-              <Flex flexDirection={"column"}>
-                <Text fontSize={"xs"} mt="1">
-                  {format(new Date(date), "MMM dd, yyyy.")}
+    <>
+      <Box mt="6" bg="gray.700" p="4" borderRadius={"4"}>
+        <Flex justifyContent="space-between">
+          <Box onClick={onOpen} cursor="pointer">
+            <Flex>
+              <Avatar name={name} />
+              <Box pl="2" pt="1">
+                <Text fontSize={"sm"}>{name}</Text>
+                <Text fontSize={"xs"} color="skyblue">
+                  {symptom}
                 </Text>
-                <Text fontSize={"sm"} mt="1">
-                  {format(parseISO(time), "h:mm a")}
-                </Text>
-              </Flex>
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
+              </Box>
+            </Flex>
+          </Box>
+          <Box pl="5">
+            <Flex alignItems={"center"}>
+              {isPastAppointment ? (
+                <Icon
+                  cursor={"pointer"}
+                  onClick={updateAppointment}
+                  color={approve ? "green.300" : "red.300"}
+                  as={FaCheckCircle}
+                  w={8}
+                  h={8}
+                  mt="1.5"
+                />
+              ) : (
+                <DoctorsPatientBadge approve={approve} />
+              )}
+              <Box ml="6">
+                <Flex flexDirection={"column"}>
+                  <Text fontSize={"xs"} mt="1">
+                    {format(new Date(date), "MMM dd, yyyy.")}
+                  </Text>
+                  <Text fontSize={"sm"} mt="1">
+                    {format(parseISO(time), "h:mm a")}
+                  </Text>
+                </Flex>
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+      <DoctorPatientModal
+        name={name}
+        symptom={symptom}
+        time={time}
+        approve={approve}
+        date={date}
+        details={details}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </>
   );
 };
